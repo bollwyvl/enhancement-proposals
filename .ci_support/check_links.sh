cc7d31c3e9b991665d313e653bebd4aedbaff8dc
Nicholas Bollweg <nick.bollweg@gmail.com>
2020-06-18 23:14:15 -0400

    more work on script output

--- .ci_support/check_links.sh
+++ .ci_support/check_links.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-set -eux
+set -eu
 
-pytest-check-links \
+CACHE_NAME=$(pwd)/.pytest-check-links-cache
+
+cd _build/html
+
+pytest \
+  --check-links \
   --check-links-cache \
+  --check-links-cache-name $CACHE_NAME \
   --check-links-cache-expire-after 604800 \
   --links-ext html \
-  _build
--- .ci_support/check_spelling.sh
+++ .ci_support/check_spelling.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
-set -eux
+set -eu
+
+echo "checking spelling..."
 
 cd _build/html
 
 hunspell \
-  -d en-GB,en_US
-  -p ../../.ci_support/dictionary.txt
-  -l
-  -H **/*.html
-  | sort
-  | uniq
+  -d en-GB,en_US \
+  -p ../../.ci_support/dictionary.txt \
+  -l \
+  -H **/*.html \
+  | sort \
+  | uniq \
   > check-spelling.txt
 
 if [ -s "check-spelling.txt" ]; then
+  echo "hunspell found misspelled words"
   cat check-spelling.txt
   exit 1
+else
+  echo "hunspell did not find any misspelled words"
 fi
